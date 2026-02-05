522e8e940ac7aaf648c567a57c189b7fb7d568ce
Justin <justinjohntilley@gmail.com>
2026-02-05 18:03:18 +1100

    Add PillrTests target references

---
+++ PillrTests/PillrTests.swift
@@ -0,0 +1,16 @@
+//
+//  PillrTests.swift
+//  PillrTests
+//
+//  Created by Justin Tilley on 5/2/2026.
+//
+
+import Testing
+
+struct PillrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
